8ba3c893297244b0f118f562ebe593bd76ec7800
ashwinn-si <ashwin23cse@gmail.com>
2024-05-21 20:49:25 +0530

    final_commits

---
+++ index.js
@@ -0,0 +1,10 @@
+var yes = document.getElementById("yes_button");
+var no = document.getElementById("no_button");
+var size = 20;
+document.getElementById("no_button").onclick = function () {
+    size += 10;
+    yes.style.fontSize = size + 'px';
+}
+document.getElementById("yes_button").onclick = function () {
+    alert("dankssss")
+}
