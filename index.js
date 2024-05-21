57130ab8dbd5ac0b7af981f7c745af4df6175829
ashwinn-si <ashwin23cse@gmail.com>
2024-05-21 20:56:23 +0530

    finalss

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var yes = document.getElementById("yes_button");
 var no = document.getElementById("no_button");
 var size = 20;
 document.getElementById("no_button").onclick = function () {
-    size += 10;
+    size += 40;
     yes.style.fontSize = size + 'px';
 }
 document.getElementById("yes_button").onclick = function () {
