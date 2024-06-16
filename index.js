729a0e258b94a4bc5e3fc73f0eeb9437d7c7f72d
ashwinn-si <ashwin23cse@gmail.com>
2024-06-16 11:04:47 +0530

    few_more_changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var yes = document.getElementById("yes_button");
 var no = document.getElementById("no_button");
 var size = 20;
-const no_content = ["enaku unna romba pudikum😓", "yes tha solla", "plss🤒🤒", "romba tha panura nee😖", "pooi duuu na pasamata🤐", "ana nee romba panura😤", "purinjikoo yaaa😒", "enakum oru time varum😌"]
+const no_content = ["enaku unna romba pudikum😓", "yes tha solla", "plss🤒🤒", "romba tha panura nee😖", "pooi duuu na pasamata🤐", "ana nee romba panura😤", "purinjikoo yaaa😒", "enakum oru time varum😌","romba scene podura nee🙄","nalla va irruka mataa pooo😭😭","unakula marriage ha agathu"];
 no_click = 0;
 document.getElementById("no_button").onclick = function () {
     size += 40;
     yes.style.fontSize = size + 'px';
-    if (no_click <= 7) {
+    if (no_click <= 10) {
         no.innerHTML = no_content[no_click];
         no_click++;
     }
