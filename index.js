537803ecd0bc913b3fa63c652746c4de62a11a7a
ashwinn-si <ashwin23cse@gmail.com>
2024-06-16 11:02:23 +0530

    FEW_commits

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var yes = document.getElementById("yes_button");
 var no = document.getElementById("no_button");
 var size = 20;
+const no_content = ["enaku unna romba pudikum😓", "yes tha solla", "plss🤒🤒", "romba tha panura nee😖", "pooi duuu na pasamata🤐", "ana nee romba panura😤", "purinjikoo yaaa😒", "enakum oru time varum😌"]
+no_click = 0;
 document.getElementById("no_button").onclick = function () {
     size += 40;
     yes.style.fontSize = size + 'px';
+    if (no_click <= 7) {
+        no.innerHTML = no_content[no_click];
+        no_click++;
+    }
+    else {
+        no.innerHTML = "😭😭";
+    }
 }
-document.getElementById("yes_button").onclick = function () {
-    
-    alert("dankssss")
+function load(){
+    window.location.href = "newpage.html"
 }
